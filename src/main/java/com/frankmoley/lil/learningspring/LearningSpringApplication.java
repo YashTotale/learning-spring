5ffc9dd1f556c1fd328a41f38fc2feee8f16418c
YashTotale <totaleyash@gmail.com>
2020-09-02 10:22:01 -0700

    project, db set up

---
+++ src/main/java/com/frankmoley/lil/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringApplication.class, args);
+    }
+
+}
